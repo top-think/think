8cabd71a4b963a08a34ac060b048fa65b862be6c
thinkphp <thinkphp@qq.com>
2013-03-28 13:49:08 +0800

    修正Cx标签库的notbetween标签解析

--- thinkphp/Think/Template/Driver/Cx.php
+++ thinkphp/Think/Template/Driver/Cx.php
@@ -341,7 +341,7 @@ class Cx extends TagLib {
         if($type=='between') {
             $parseStr = '<?php $_RANGE_VAR_='.$str.';if('.$name.'>= $_RANGE_VAR_[0] && '.$name.'<= $_RANGE_VAR_[1]):?>'.$content.'<?php endif; ?>';
         }elseif($type=='notbetween'){
-            $parseStr = '<?php $_RANGE_VAR_='.$str.';if('.$name.'<$_RANGE_VAR_[0] && '.$name.'>$_RANGE_VAR_[1]):?>'.$content.'<?php endif; ?>';
+            $parseStr = '<?php $_RANGE_VAR_='.$str.';if('.$name.'<$_RANGE_VAR_[0] || '.$name.'>$_RANGE_VAR_[1]):?>'.$content.'<?php endif; ?>';
         }else{
             $fun        =  ($type == 'in')? 'in_array'    :   '!in_array';
             $parseStr   = '<?php if('.$fun.'(('.$name.'), '.$str.')): ?>'.$content.'<?php endif; ?>';
