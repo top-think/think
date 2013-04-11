fdee52eca2bd59ccf2b6094c21ed6ba09956e458
thinkphp <thinkphp@qq.com>
2013-04-11 16:48:41 +0800

    Log类改进

--- thinkphp/Think/Log.php
+++ thinkphp/Think/Log.php
@@ -21,8 +21,12 @@ class Log {
     static public function init($config=[]){
         $type   =   isset($config['type'])?$config['type']:'File';
         $class  =   'Think\\Log\\Driver\\'. ucwords($type);
-        unset($config['type']);
-        self::$storage = new $class($config);
+        if(class_exists($class)) {
+            unset($config['type']);
+            self::$storage = new $class($config);
+        }else{
+            throw new \Exception('Log type not exists:'.$type);
+        }
     }
 
     /**
@@ -67,7 +71,7 @@ class Log {
             }
             self::$log = [];
         }
-        self::$storage->write($message,$destination);
+        self::$storage && self::$storage->write($message,$destination);
     }
 
     /**
@@ -79,7 +83,7 @@ class Log {
      * @return void
      */
     static public function write($log,$level,$destination='') {
-        self::$storage->write("{$level}: {$log}",$destination);
+        self::$storage && self::$storage->write("{$level}: {$log}",$destination);
     }
 
 }
