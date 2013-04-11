76260037c8a91ec163d5af1972f9e4e2d3ad2fac
thinkphp <thinkphp@qq.com>
2013-04-11 17:31:43 +0800

    惯例配置调整

--- thinkphp/convention.php
+++ thinkphp/convention.php
@@ -22,8 +22,8 @@ return [
     'pathinfo_depr'     =>  '/',    // pathinfo分隔符
     'require_module'    =>  true,   // 是否显示模块
     'default_module'    =>  'index',  // 默认模块名
-    'require_controller'  =>  true,   // 是否显示控制器
-    'default_controller'  =>  'index',    // 默认控制器名
+    'require_controller'=>  true,   // 是否显示控制器
+    'default_controller'=>  'index',    // 默认控制器名
     'default_action'    =>  'index',    // 默认操作名
     'action_suffix'     =>  '', // 操作方法后缀
     'url_model'         =>  1,  // URL模式
@@ -31,26 +31,29 @@ return [
     'url_html_suffix'   =>  '.html',
     'url_params_bind'   =>  false,  // url变量绑定
     'exception_tmpl'    =>  THINK_PATH.'Tpl/think_exception.tpl',// 异常页面的模板文件
-    'tmpl_action_error'     => THINK_PATH.'Tpl/dispatch_jump.tpl', // 默认错误跳转对应的模板文件
-    'tmpl_action_success'   => THINK_PATH.'Tpl/dispatch_jump.tpl', // 默认成功跳转对应的模板文件
+    'error_tmpl'        =>  THINK_PATH.'Tpl/dispatch_jump.tpl', // 默认错误跳转对应的模板文件
+    'success_tmpl'      =>  THINK_PATH.'Tpl/dispatch_jump.tpl', // 默认成功跳转对应的模板文件
+    'default_ajax_return'   => 'JSON',  // 默认AJAX 数据返回格式,可选JSON XML ...
+    'default_jsonp_handler' => 'jsonpReturn', // 默认JSONP格式返回的处理方法
+	'var_jsonp_handler'     => 'callback',
 
     /* 错误设置 */
-    'error_message'         => '页面错误！请稍后再试～',//错误显示信息,非调试模式有效
-    'error_page'            => '',	// 错误定向页面
-    'show_error_msg'        => false,    // 显示错误信息
+    'error_message'     =>  '页面错误！请稍后再试～',//错误显示信息,非调试模式有效
+    'error_page'        =>  '',	// 错误定向页面
+    'show_error_msg'    =>  false,    // 显示错误信息
 
     /* 数据库设置 */
-    'db_type'               => 'mysql',     // 数据库类型
-    'db_host'               => 'localhost', // 服务器地址
-    'db_name'               => '',          // 数据库名
-    'db_user'               => 'root',      // 用户名
-    'db_pwd'                => '',          // 密码
-    'db_port'               => '',        // 端口
-    'db_prefix'             => 'think_',    // 数据库表前缀
-    'db_fields_cache'       => true,        // 启用字段缓存
-    'db_charset'            => 'utf8',      // 数据库编码默认采用utf8
-    'db_deploy_type'        => 0, // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-    'db_rw_separate'        => false,       // 数据库读写是否分离 主从式有效
-    'db_master_num'         => 1, // 读写分离后 主服务器数量
-    'db_slave_no'           => '', // 指定从服务器序号
+    'db_type'           =>  'mysql',     // 数据库类型
+    'db_host'           =>  'localhost', // 服务器地址
+    'db_name'           =>  '',          // 数据库名
+    'db_user'           =>  'root',      // 用户名
+    'db_pwd'            =>  '',          // 密码
+    'db_port'           =>  '',        // 端口
+    'db_prefix'         =>  'think_',    // 数据库表前缀
+    'db_fields_cache'   =>  true,        // 启用字段缓存
+    'db_charset'        =>  'utf8',      // 数据库编码默认采用utf8
+    'db_deploy_type'    =>  0, // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
+    'db_rw_separate'    =>  false,       // 数据库读写是否分离 主从式有效
+    'db_master_num'     =>  1, // 读写分离后 主服务器数量
+    'db_slave_no'       =>  '', // 指定从服务器序号
 ];
