47f96585544b67826ca620fe2a8443d1450d0f7c
麦当苗儿 <zuojiazi@vip.qq.com>
2013-04-15 12:19:20 +0800

    代码格式规范调整

--- thinkphp/Library/Org/Crypt.php
+++ thinkphp/Library/Org/Crypt.php
@@ -20,22 +20,22 @@ class Crypt {
      * @return string
      */
     static public function encrypt($data,$key,$expire=0){
-        $key    =   md5($key);
-        $data   =   base64_encode($data);
-        $x=0;
-        $len = strlen($data);
-        $l = strlen($key);
-        $char =  '';
-        for ($i=0;$i< $len;$i++) {
-            if ($x== $l) $x=0;
-            $char   .=substr($key,$x,1);
+        $key  = md5($key);
+        $data = base64_encode($data);
+        $x    = 0;
+        $len  = strlen($data);
+        $l    = strlen($key);
+        $char = '';
+        for ($i = 0; $i< $len; $i++) {
+            if ($x == $l) $x = 0;
+            $char .=substr($key, $x, 1);
             $x++;
         }
-        $str    =   sprintf('%010d', $expire ? $expire + time():0);
-        for ($i=0;$i< $len;$i++) {
-            $str    .=chr(ord(substr($data,$i,1))+(ord(substr($char,$i,1)))%256);
+        $str = sprintf('%010d', $expire ? $expire + time() : 0);
+        for ($i=0; $i< $len; $i++) {
+            $str .= chr(ord(substr($data, $i, 1)) + (ord(substr($char, $i, 1))) % 256);
         }
-        return str_replace('=', '',base64_encode($str));
+        return str_replace('=', '', base64_encode($str));
     }
 
     /**
@@ -46,27 +46,27 @@ class Crypt {
      * @return string
      */
     static public function decrypt($data,$key){
-        $key    =   md5($key);
-        $x=0;
-        $data   =   base64_decode($data);
-        $expire =   substr($data,0,10);
-        $data   =   substr($data,10);
-        if($expire>0 && $expire<time()) {
+        $key    = md5($key);
+        $x      = 0;
+        $data   = base64_decode($data);
+        $expire = substr($data,0,10);
+        $data   = substr($data,10);
+        if($expire > 0 && $expire<time()) {
             return '';
         }
-        $len = strlen($data);
-        $l = strlen($key);    
-        $char =     $str = '';
-        for ($i=0;$i< $len;$i++) {
-            if ($x== $l) $x=0;
-            $char   .=substr($key,$x,1);
+        $len  = strlen($data);
+        $l    = strlen($key);    
+        $char = $str = '';
+        for ($i=0; $i< $len; $i++) {
+            if ($x == $l) $x = 0;
+            $char .= substr($key, $x, 1);
             $x++;
         }
-        for ($i=0;$i< $len;$i++) {
-            if (ord(substr($data,$i,1))<ord(substr($char,$i,1))) {
-                $str    .=chr((ord(substr($data,$i,1))+256)-ord(substr($char,$i,1)));
+        for ($i = 0; $i < $len; $i++) {
+            if (ord(substr($data, $i, 1)) < ord(substr($char, $i, 1))) {
+                $str .= chr((ord(substr($data, $i, 1)) + 256) - ord(substr($char, $i, 1)));
             }else{
-                $str    .=chr(ord(substr($data,$i,1))-ord(substr($char,$i,1)));
+                $str .= chr(ord(substr($data, $i, 1)) - ord(substr($char, $i, 1)));
             }
         }
         return base64_decode($str);
