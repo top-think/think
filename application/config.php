14ebc746aa9a8f6c434493f88632b6d65499e890
麦当苗儿 <zuojiazi@vip.qq.com>
2013-04-16 11:00:24 +0800

    代码规范调整

--- application/config.php
+++ application/config.php
@@ -8,29 +8,28 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
-// $Id$
 
 // 惯例配置文件
 return [
-    'app_debug'         =>  true,
-    'var_module'        =>  'm',
-    'var_controll'      =>  'c',
-    'var_action'        =>  'a',
-    'var_pathinfo'      =>  's',
-    'pathinfo_fetch'    =>  'ORIG_PATH_INFO,REDIRECT_PATH_INFO,REDIRECT_URL',
-    'pathinfo_depr'     =>  '/',
-    'require_module'    =>  true,
-    'default_module'    =>  'index',
-    'require_controller'  =>  true,
-    'default_controller'  =>  'index',
-    'lib_path'          =>  '',
-    'app_path'          =>  '',
-    'action_suffix'     =>  '',
-    'file_ext'          =>  '.php',
-    'url_model'         =>  1,
-    'TMPL_EXCEPTION_FILE'   => THINK_PATH.'tpl/think_exception.tpl',// 异常页面的模板文件
-    'show_page_trace'       =>  true,
-        'TRACE_PAGE_TABS'   => array('BASE'=>'基本','FILE'=>'文件','INFO'=>'流程','ERR|NOTIC'=>'错误','SQL'=>'SQL','DEBUG'=>'调试'), // 页面Trace可定制的选项卡 
-        'db_sql_log'=>true,
-        'url_params_bind'=>TRUE,
+    'app_debug'           => true,
+    'var_module'          => 'm',
+    'var_controll'        => 'c',
+    'var_action'          => 'a',
+    'var_pathinfo'        => 's',
+    'pathinfo_fetch'      => 'ORIG_PATH_INFO,REDIRECT_PATH_INFO,REDIRECT_URL',
+    'pathinfo_depr'       => '/',
+    'require_module'      => true,
+    'default_module'      => 'index',
+    'require_controller'  => true,
+    'default_controller'  => 'index',
+    'lib_path'            => '',
+    'app_path'            => '',
+    'action_suffix'       => '',
+    'file_ext'            => '.php',
+    'url_model'           => 1,
+    'TMPL_EXCEPTION_FILE' => THINK_PATH.'tpl/think_exception.tpl',// 异常页面的模板文件
+    'show_page_trace'     => true,
+    'trace_page_tabs'     => array('BASE'=>'基本','FILE'=>'文件','INFO'=>'流程','ERR|NOTIC'=>'错误','SQL'=>'SQL','DEBUG'=>'调试'), // 页面Trace可定制的选项卡 
+    'db_sql_log'          => true,
+    'url_params_bind'     => true,
 ];
