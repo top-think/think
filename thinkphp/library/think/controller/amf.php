0efd459c172ca4fff7ecfc97c98d36607342f4ac
huangdijia <huangdijia@gmail.com>
2015-05-04 17:36:03 +0800

    增加文件

---
+++ thinkphp/library/think/controller/amf.php
@@ -0,0 +1,30 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2015 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+namespace think\controller;
+
+abstract class amf {
+
+   /**
+     * PHPRpc控制器架构函数
+     * @access public
+     */
+    public function __construct() {
+    	//导入类库
+    	think\loader::import('Vendor.Zend.Amf.Server');
+    	//实例化AMF
+    	$server = new \Zend_Amf_Server();
+  	    $server -> setClass($this);
+    	echo $server -> handle();
+        return ;
+    }
+
+}
