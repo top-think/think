419972306d1746d0ed69994563d20ca4fceb6a15
thinkphp <thinkphp@qq.com>
2015-12-10 13:54:12 +0800

    去除调试代码

--- application/config.php
+++ application/config.php
@@ -10,22 +10,17 @@
 // +----------------------------------------------------------------------
 // $Id$
 
-
-think\Route::get('new/:id','New/read'); // 定义GET请求路由规则
-think\Route::post('new/:id','New/update'); // 定义POST请求路由规则
-
 return [
     'url_route_on' => true,
-    'slog'                  => [
+    'slog'         => [
         'host'                => '111.202.76.133',
         //是否显示利于优化的参数，如果允许时间，消耗内存等
         'optimize'            => true,
         'show_included_files' => true,
         'error_handler'       => true,
         //日志强制记录到配置的client_id
-        'force_client_id'     => isset($_REQUEST['slog_force_client_id'])?$_REQUEST['slog_force_client_id']:'',
+        'force_client_id'     => isset($_REQUEST['slog_force_client_id']) ? $_REQUEST['slog_force_client_id'] : '',
         //限制允许读取日志的client_id
         'allow_client_ids'    => array(),
     ],
 ];
-
