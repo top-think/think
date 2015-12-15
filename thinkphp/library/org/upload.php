2a21d309bb5cd7dbe2319466dc1ccb0876227339
huangdijia <huangdijia@gmail.com>
2015-12-15 14:45:41 +0800

    优化注释

--- thinkphp/library/org/upload.php
+++ thinkphp/library/org/upload.php
@@ -17,20 +17,34 @@ class Upload
      * @var array
      */
     private $config = [
-        'mimes'        => [], //允许上传的文件MiMe类型
-        'maxSize'      => 0, //上传的文件大小限制 (0-不做限制)
-        'exts'         => [], //允许上传的文件后缀
-        'autoSub'      => true, //自动子目录保存文件
-        'subName'      => ['date', 'Y-m-d'], //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
-        'rootPath'     => './Uploads/', //保存根路径
-        'savePath'     => '', //保存路径
-        'saveName'     => ['uniqid', ''], //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
-        'saveExt'      => '', //文件保存后缀，空则使用原后缀
-        'replace'      => false, //存在同名是否覆盖
-        'hash'         => true, //是否生成hash编码
-        'callback'     => false, //检测文件是否存在回调，如果存在返回文件信息数组
-        'driver'       => '', // 文件上传驱动
-        'driverConfig' => [], // 上传驱动配置
+        // 允许上传的文件MiMe类型
+        'mimes'        => [],
+        // 上传的文件大小限制 (0-不做限制) 
+        'maxSize'      => 0, 
+        // 允许上传的文件后缀
+        'exts'         => [], 
+        // 自动子目录保存文件
+        'autoSub'      => true, 
+        // 子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
+        'subName'      => ['date', 'Y-m-d'], 
+        //保存根路径
+        'rootPath'     => './Uploads/',
+        // 保存路径 
+        'savePath'     => '', 
+        // 上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
+        'saveName'     => ['uniqid', ''], 
+        // 文件保存后缀，空则使用原后缀
+        'saveExt'      => '', 
+        // 存在同名是否覆盖
+        'replace'      => false,
+        // 是否生成hash编码 
+        'hash'         => true, 
+        // 检测文件是否存在回调，如果存在返回文件信息数组
+        'callback'     => false,
+        // 文件上传驱动e, 
+        'driver'       => '', 
+        // 上传驱动配置
+        'driverConfig' => [], 
     ];
 
     /**
