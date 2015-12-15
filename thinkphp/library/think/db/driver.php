e7fc857a9b2b92f183c9c9edc2e1a21157654e69
thinkphp <thinkphp@qq.com>
2015-12-15 20:08:53 +0800

    修正数据库驱动类的quote方法

--- thinkphp/library/think/db/driver.php
+++ thinkphp/library/think/db/driver.php
@@ -40,22 +40,22 @@ abstract class Driver
     protected $_linkID = null;
     // 数据库连接参数配置
     protected $config = [
-        'db_like_fields'=>'', //like字段自动替换为%%包裹
-        'type'        => '', // 数据库类型
-        'hostname'    => '127.0.0.1', // 服务器地址
-        'database'    => '', // 数据库名
-        'username'    => '', // 用户名
-        'password'    => '', // 密码
-        'hostport'    => '', // 端口
-        'dsn'         => '', //
-        'params'      => [], // 数据库连接参数
-        'charset'     => 'utf8', // 数据库编码默认采用utf8
-        'prefix'      => '', // 数据库表前缀
-        'debug'       => false, // 数据库调试模式
-        'deploy'      => 0, // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-        'rw_separate' => false, // 数据库读写是否分离 主从式有效
-        'master_num'  => 1, // 读写分离后 主服务器数量
-        'slave_no'    => '', // 指定从服务器序号
+        'db_like_fields' => '', //like字段自动替换为%%包裹
+        'type'           => '', // 数据库类型
+        'hostname'       => '127.0.0.1', // 服务器地址
+        'database'       => '', // 数据库名
+        'username'       => '', // 用户名
+        'password'       => '', // 密码
+        'hostport'       => '', // 端口
+        'dsn'            => '', //
+        'params'         => [], // 数据库连接参数
+        'charset'        => 'utf8', // 数据库编码默认采用utf8
+        'prefix'         => '', // 数据库表前缀
+        'debug'          => false, // 数据库调试模式
+        'deploy'         => 0, // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
+        'rw_separate'    => false, // 数据库读写是否分离 主从式有效
+        'master_num'     => 1, // 读写分离后 主服务器数量
+        'slave_no'       => '', // 指定从服务器序号
     ];
     // 数据库表达式
     protected $exp = ['eq' => '=', 'neq' => '<>', 'gt' => '>', 'egt' => '>=', 'lt' => '<', 'elt' => '<=', 'notlike' => 'NOT LIKE', 'like' => 'LIKE', 'in' => 'IN', 'notin' => 'NOT IN', 'not in' => 'NOT IN', 'between' => 'BETWEEN', 'not between' => 'NOT BETWEEN', 'notbetween' => 'NOT BETWEEN'];
@@ -1128,6 +1128,7 @@ abstract class Driver
      */
     public function quote($str)
     {
+        $this->initConnect();
         return $this->_linkID ? $this->_linkID->quote($str) : $str;
     }
 
