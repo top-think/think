a7611a66455580130301721a950ebf7f8e14b0ae
7IN0SAN9 <me@7in0.me>
2015-12-18 01:41:57 +0800

    创建测试入口 mock.php

---
+++ tests/mock.php
@@ -0,0 +1,24 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2015 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// 测试入口文件
+
+define('IN_UNIT_TEST', true);
+
+$_SERVER['REQUEST_METHOD'] = 'GET';
+
+// 定义项目路径
+define('APP_PATH', __DIR__ . '/../application/');
+// 开启调试模式
+define('APP_DEBUG', true);
+
+// 加载框架引导文件
+require __DIR__ . '/../thinkphp/start.php';
