d7215610451db4e7dcb868c4b98a62b231a6f7a9
Haotong Lin <lofanmi@gmail.com>
2015-12-29 20:58:47 +0800

    app单元测试

---
+++ tests/framework/thinkphp/library/think/appTest.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2015 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: Haotong Lin <lofanmi@gmail.com>
+// +----------------------------------------------------------------------
+
+namespace think;
+
+class appTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRun()
+    {
+        \think\App::run();
+        // todo...
+    }
+}
