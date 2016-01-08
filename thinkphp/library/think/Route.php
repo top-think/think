d9b957299f8e189ed466519e56721cb7bee59944
小陈叔叔 <chenjxlg@163.com>
2016-01-08 11:25:34 +0800

    如果分组路由为空的时候,不进行路由注册

--- thinkphp/library/think/Route.php
+++ thinkphp/library/think/Route.php
@@ -116,6 +116,7 @@ class Route
                         $key = array_shift($val);
                     }
                     if (0 === strpos($key, '[')) {
+                        if (empty($val)) break;
                         self::$rules[$type][substr($key, 1, -1)] = ['routes' => $val, 'option' => $option, 'pattern' => $pattern];
                     } elseif (is_array($val)) {
                         self::$rules[$type][$key] = ['route' => $val[0], 'option' => $val[1], 'pattern' => $val[2]];
