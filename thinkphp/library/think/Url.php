ed87fa9e2c94ddbbf8fc374c579ae4dd6d27682f
thinkphp <thinkphp@qq.com>
2016-01-10 17:08:36 +0800

    注释调整

--- thinkphp/library/think/Url.php
+++ thinkphp/library/think/Url.php
@@ -14,14 +14,14 @@ namespace think;
 class Url
 {
     /**
-     * URL组装 支持不同URL模式
+     * URL生成 支持路由反射
      * @param string $url URL表达式，
      * 格式：'[模块/控制器/操作]?参数1=值1&参数2=值2...'
      * @控制器/操作?参数1=值1&参数2=值2...
      * \\命名空间类\\方法?参数1=值1&参数2=值2...
      * @param string|array $vars 传入的参数，支持数组和字符串
      * @param string $suffix 伪静态后缀，默认为true表示获取配置值
-     * @param boolean $domain 是否显示域名
+     * @param boolean|string $domain 是否显示域名 或者直接传入域名
      * @return string
      */
     public static function build($url = '', $vars = '', $suffix = true, $domain = true)
@@ -125,6 +125,7 @@ class Url
 
         if ($domain) {
             if (true === $domain) {
+                // 自动判断域名
                 $domain = $_SERVER['HTTP_HOST'];
                 if (Config::get('url_domain_deploy')) {
                     // 开启子域名部署
