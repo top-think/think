1d61d6c3ebea5e333a027a69380bdbabe877fe86
7IN0SAN9 <me@7in0.me>
2016-01-12 16:57:41 +0800

    不通过pecl自动加载so文件

--- tests/script/install.sh
+++ tests/script/install.sh
@@ -4,10 +4,10 @@ if [ $(phpenv version-name) != "hhvm" ]; then
     pecl channel-update pecl.php.net
 
     if [ $(phpenv version-name) = "7.0" ]; then
+        pear config-set php_ini ''
+        pecl config-set php_ini ''
         echo "yes\nno\n" | pecl install apcu-5.1.2
         pecl install apcu_bc-beta
-        sed -n '/apcu.so/!p' ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-        sed -n '/apc.so/!p' ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
         phpenv config-add tests/conf/apcu_bc.ini
     else
         echo "yes\nno\n" | pecl install apcu-4.0.10
