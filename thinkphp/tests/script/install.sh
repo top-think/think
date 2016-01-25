4b0d46fd184c9ef04de133a488cee36d03b45fd9
7IN0SAN9 <me@7in0.me>
2016-01-25 17:37:11 +0800

    调整测试用文件结构

--- thinkphp/tests/script/install.sh
+++ thinkphp/tests/script/install.sh
@@ -3,17 +3,17 @@
 if [ $(phpenv version-name) != "hhvm" ]; then
     pecl channel-update pecl.php.net
 
+    pear config-set php_ini ''
+    pecl config-set php_ini ''
+
     if [ $(phpenv version-name) = "7.0" ]; then
-        pear config-set php_ini ''
-        pecl config-set php_ini ''
         echo "yes\nno\n" | pecl install apcu-5.1.2
         pecl install apcu_bc-beta
         phpenv config-add thinkphp/tests/conf/apcu_bc.ini
     else
         echo "yes\nno\n" | pecl install apcu-4.0.10
+        phpenv config-add thinkphp/tests/conf/apcu.ini
     fi
-
-    phpenv config-add thinkphp/tests/conf/apc.ini
 fi
 
 composer install --no-interaction --ignore-platform-reqs
