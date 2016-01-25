09109632a70ff2bf6ea764d12eca16440d62cc91
Chino Chang <me@7in0.me>
2016-01-25 15:34:55 +0800

    fix CI faild

--- thinkphp/library/think/session/Driver.php
+++ thinkphp/library/think/session/Driver.php
@@ -9,7 +9,7 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-namespace think\session\driver;
+namespace think\session;
 
 use SessionHandler;
 
