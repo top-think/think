01f1c99ba2536a8bc8cfc62184bfc59b471f461f
huangdijia <huangdijia@gmail.com>
2016-01-26 13:56:54 +0800

    原cache驱动Simple改名Lite，增加有效期支持

--- thinkphp/library/think/cache/driver/Lite.php
+++ thinkphp/library/think/cache/driver/Lite.php
@@ -17,12 +17,13 @@ use think\Cache;
  * 文件类型缓存类
  * @author    liu21st <liu21st@gmail.com>
  */
-class Simple
+class Lite
 {
 
     protected $options = [
         'prefix' => '',
         'path'   => '',
+        'expire' => 0, // 等于 10*365*24*3600（10年）
     ];
 
     /**
@@ -64,6 +65,13 @@ class Simple
         Cache::$readTimes++;
         $filename = $this->filename($name);
         if (is_file($filename)) {
+            // 判断是否过期
+            $mtime = filemtime($filename);
+            if ($mtime<time()) {
+                // 清除已经过期的文件
+                unlink($filename);
+                return false;
+            }
             return include $filename;
         } else {
             return false;
@@ -76,20 +84,34 @@ class Simple
      *
      * @param string $name  缓存变量名
      * @param mixed  $value 存储数据
+     * @internal param int $expire 有效时间 0为永久
      *
      * @return bool
-     * @internal param int $expire 有效时间 0为永久
      */
-    public function set($name, $value)
+    public function set($name, $value, $expire=null)
     {
         Cache::$writeTimes++;
+        if (is_null($expire)) {
+            $expire = $this->options['expire'];
+        }
+        // 模拟永久
+        if (0===$expire) {
+            $expire = 10*365*24*3600;
+        }
         $filename = $this->filename($name);
         // 缓存数据
+        /*
         $dir = dirname($filename);
         // 目录不存在则创建
-        //if (!is_dir($dir))
-        //  mkdir($dir,0755,true);
-        return file_put_contents($filename, ("<?php\treturn " . var_export($value, true) . ";?>"));
+        if (!is_dir($dir))
+            mkdir($dir,0755,true);
+        */
+        $ret = file_put_contents($filename, ("<?php\treturn " . var_export($value, true) . ";?>"));
+        // 通过设置修改时间实现有效期
+        if ($ret) {
+            touch($filename, time()+$expire);
+        }
+        return $ret;
     }
 
     /**
