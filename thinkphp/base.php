bf76a28368e222a775128d0aff35b277677be0fb
thinkphp <thinkphp@qq.com>
2016-01-27 16:03:30 +0800

    NOW_TIME 常量 更改为 $_SERVER['REQUEST_TIME']

--- thinkphp/base.php
+++ thinkphp/base.php
@@ -52,7 +52,7 @@ define('IS_CGI', strpos(PHP_SAPI, 'cgi') === 0 ? 1 : 0);
 define('IS_WIN', strstr(PHP_OS, 'WIN') ? 1 : 0);
 define('IS_CLI', PHP_SAPI == 'cli' ? 1 : 0);
 define('IS_AJAX', (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') ? true : false);
-define('NOW_TIME', $_SERVER['REQUEST_TIME_FLOAT']);
+define('NOW_TIME', $_SERVER['REQUEST_TIME']);
 define('REQUEST_METHOD', IS_CLI ? 'GET' : $_SERVER['REQUEST_METHOD']);
 define('IS_GET', REQUEST_METHOD == 'GET' ? true : false);
 define('IS_POST', REQUEST_METHOD == 'POST' ? true : false);
