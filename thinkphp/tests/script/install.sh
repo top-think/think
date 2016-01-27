4b42a6ebc96110d3e51bfbfda45f65d72558c65b
7IN0SAN9 <me@7in0.me>
2016-01-27 22:05:30 +0800

    添加Redis和Memcached扩展

--- thinkphp/tests/script/install.sh
+++ thinkphp/tests/script/install.sh
@@ -10,10 +10,21 @@ if [ $(phpenv version-name) != "hhvm" ]; then
         echo "yes\nno\n" | pecl install apcu-5.1.2
         pecl install apcu_bc-beta
         phpenv config-add thinkphp/tests/conf/apcu_bc.ini
+
+        wget -O phpredis-php7.tar.gz https://github.com/phpredis/phpredis/archive/php7.tar.gz
+        tar -xzvf phpredis-php7.tar.gz
+        cd phpredis-php7 && phpize && ./configure && make && sudo make install
+
+        wget -O memcached-php7.tar.gz https://github.com/php-memcached-dev/php-memcached/archive/php7.zip
+        tar -xzvf memcached-php7.tar.gz
+        cd php-memcached-php7 && phpize && ./configure && make && sudo make install
     else
         echo "yes\nno\n" | pecl install apcu-4.0.10
         phpenv config-add thinkphp/tests/conf/apcu.ini
     fi
 fi
+
+phpenv config-add thinkphp/tests/conf/memcached.ini
+phpenv config-add thinkphp/tests/conf/redis.ini
 
 composer install --no-interaction --ignore-platform-reqs
