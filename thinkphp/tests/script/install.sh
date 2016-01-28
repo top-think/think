29955840693f52caff575815be9833a46249a30a
7IN0SAN9 <me@7in0.me>
2016-01-28 17:18:33 +0800

    预编译二进制文件

--- thinkphp/tests/script/install.sh
+++ thinkphp/tests/script/install.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 
 if [ $(phpenv version-name) != "hhvm" ]; then
-    pecl channel-update pecl.php.net
-
-    pear config-set php_ini ''
-    pecl config-set php_ini ''
+    cp thinkphp/tests/extensions/$(phpenv version-name)/*.so $(php-config --extension-dir)
 
     if [ $(phpenv version-name) = "7.0" ]; then
-        echo "yes\nno\n" | pecl install apcu-5.1.2
-        pecl install apcu_bc-beta
         phpenv config-add thinkphp/tests/conf/apcu_bc.ini
-
-        wget -O phpredis-php7.tar.gz https://github.com/phpredis/phpredis/archive/php7.tar.gz
-        tar -xzvf phpredis-php7.tar.gz
-        cd phpredis-php7 && phpize && ./configure && make && sudo make install
-
-        wget -O memcached-php7.tar.gz https://github.com/php-memcached-dev/php-memcached/archive/php7.zip
-        tar -xzvf memcached-php7.tar.gz
-        cd php-memcached-php7 && phpize && ./configure && make && sudo make install
     else
-        echo "yes\nno\n" | pecl install apcu-4.0.10
         phpenv config-add thinkphp/tests/conf/apcu.ini
     fi
-fi
 
-phpenv config-add thinkphp/tests/conf/memcached.ini
-phpenv config-add thinkphp/tests/conf/redis.ini
+    phpenv config-add thinkphp/tests/conf/memcached.ini
+    phpenv config-add thinkphp/tests/conf/redis.ini
+fi
 
 composer install --no-interaction --ignore-platform-reqs
