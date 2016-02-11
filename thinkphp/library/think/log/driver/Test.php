c8b788cb09d19ba245bd0fa24ab7d9e52973f60e
thinkphp <thinkphp@qq.com>
2016-02-11 09:25:41 +0800

    添加Test 日志驱动

---
+++ thinkphp/library/think/log/driver/Test.php
@@ -0,0 +1,28 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+namespace think\log\driver;
+
+/**
+ * 模拟测试输出
+ */
+class Test
+{
+    /**
+     * 日志写入接口
+     * @access public
+     * @param array $log 日志信息
+     * @return void
+     */
+    public function save(array $log = [])
+    {
+    }
+
+}
