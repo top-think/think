5918edae16588506264fa098d27134a93e621dab
thinkphp <thinkphp@qq.com>
2016-02-26 14:38:28 +0800

    注释

--- build.php
+++ build.php
@@ -14,14 +14,14 @@ return [
     // 生成运行时目录
     '__dir__'  => ['runtime/cache', 'runtime/log', 'runtime/temp', 'runtime/template'],
     // 生成应用公共文件
-    '__file__' => ['common.php','config.php','database.php'],
+    '__file__' => ['common.php', 'config.php', 'database.php'],
 
-    // 定义demo模块的自动生成
-    'demo'    => [
+    // 定义demo模块的自动生成 （按照实际定义的文件名生成）
+    'demo'     => [
         '__file__'   => ['common.php'],
         '__dir__'    => ['behavior', 'controller', 'model', 'view'],
         'controller' => ['Index', 'Test', 'UserType'],
-        'model'      => ['User','UserType'],
+        'model'      => ['User', 'UserType'],
         'view'       => ['index/index'],
     ],
     // 其他更多的模块定义
