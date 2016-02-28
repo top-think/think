86810076f3dfc53c02944c8ada3f2f1314f79ef8
thinkphp <thinkphp@qq.com>
2016-02-28 22:17:26 +0800

    修正Url类

--- thinkphp/library/think/Url.php
+++ thinkphp/library/think/Url.php
@@ -40,9 +40,9 @@ class Url
                 // 解析域名
                 list($anchor, $domain) = explode('@', $anchor, 2);
             }
-        } elseif (false !== strpos($url, '@')) {
+        } elseif (strpos($url, '@')) {
             // 解析域名
-            list($url, $domain) = explode('@', $info['path'], 2);
+            list($url, $domain) = explode('@', $url, 2);
         }
 
         // 解析参数
