46a538ab57ab06a61a7c0f3c7b933dc54aa788f6
huangdijia <huangdijia@gmail.com>
2016-03-01 08:22:45 +0800

    日志增加REQUEST_METHOD

--- thinkphp/library/think/log/driver/File.php
+++ thinkphp/library/think/log/driver/File.php
@@ -72,8 +72,9 @@ class File
 
         $server = isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '0.0.0.0';
         $remote = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '0.0.0.0';
+        $method = REQUEST_METHOD;
         $uri    = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
-        return error_log("[{$now}] {$server} {$remote} {$uri}\r\n{$info}\r\n", 3, $destination);
+        return error_log("[{$now}] {$server} {$remote} {$method} {$uri}\r\n{$info}\r\n", 3, $destination);
     }
 
 }
