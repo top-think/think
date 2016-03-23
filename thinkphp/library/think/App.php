7eeecf1c7bbf97800f10ef22d67b6732553bfccf
thinkphp <thinkphp@qq.com>
2016-03-23 13:53:48 +0800

    增加应用的环境变量配置文件 增加ROOT_PATH常量定义 runtime目录默认移到ROOT_PATH录下面

--- thinkphp/library/think/App.php
+++ thinkphp/library/think/App.php
@@ -28,6 +28,9 @@ class App
      */
     public static function run()
     {
+        // 加载环境变量配置文件
+        Config::loadEnv(APP_PATH . 'env' . EXT);
+
         // 初始化应用（公共模块）
         self::initModule(COMMON_MODULE, Config::get());
 
