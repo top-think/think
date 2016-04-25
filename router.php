44f2acbb9795cb5714a4bae06510dba0ef981c17
thinkphp <thinkphp@qq.com>
2016-04-25 20:23:41 +0800

    改进router.php

--- router.php
+++ router.php
@@ -10,7 +10,7 @@
 // +----------------------------------------------------------------------
 // $Id$
 
-if (file_exists($_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"])) {
+if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"])) {
     return false;
 } else {
     require "./public/index.php";
