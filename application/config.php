321170c7cedba6d67dfcc520bb65c08411820b8e
Jim Liu <hldh214@Gmail.com>
2016-08-15 13:37:02 +0800

    add `extra_config_list` => `route`

--- application/config.php
+++ application/config.php
@@ -27,7 +27,7 @@ return [
     // 注册的根命名空间
     'root_namespace'         => [],
     // 扩展配置文件
-    'extra_config_list'      => ['database', 'validate'],
+    'extra_config_list'      => ['database', 'validate', 'route'],
     // 扩展函数文件
     'extra_file_list'        => [THINK_PATH . 'helper' . EXT],
     // 默认输出类型
