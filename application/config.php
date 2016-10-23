54372d68a10a8a60f48ac538a478a2456f28ad01
thinkphp <thinkphp@qq.com>
2016-10-24 07:11:37 +0800

    配置调整

--- application/config.php
+++ application/config.php
@@ -90,6 +90,8 @@ return [
     'url_param_type'         => 0,
     // 是否开启路由
     'url_route_on'           => true,
+    // 路由使用完整匹配
+    'route_complete_match'   => false,
     // 路由配置文件（支持配置多个）
     'route_config_file'      => ['route'],
     // 是否强制使用路由
