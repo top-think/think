d94475cf8bdb54ba2fe280a69b553f5efecdcc9b
thinkphp <thinkphp@qq.com>
2017-07-29 08:39:23 +0800

    build文件修改

--- build.php
+++ build.php
@@ -2,7 +2,7 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006~2017 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
@@ -11,7 +11,7 @@
 
 return [
     // 生成应用公共文件
-    '__file__' => ['common.php', 'config.php', 'database.php'],
+    '__file__' => ['common.php'],
 
     // 定义demo模块的自动生成 （按照实际定义的文件名生成）
     'demo'     => [
@@ -21,5 +21,6 @@ return [
         'model'      => ['User', 'UserType'],
         'view'       => ['index/index'],
     ],
+
     // 其他更多的模块定义
 ];
