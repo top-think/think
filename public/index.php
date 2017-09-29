d9317ee834e02b297a805f515011257ffb4c9bf9
thinkphp <thinkphp@qq.com>
2017-09-29 11:07:33 +0800

    调整应用和命令行入口文件

--- public/index.php
+++ public/index.php
@@ -10,6 +10,12 @@
 // +----------------------------------------------------------------------
 
 // [ 应用入口文件 ]
+namespace think;
 
-// 加载框架引导文件
-require __DIR__ . '/../thinkphp/start.php';
+// 加载基础文件
+require __DIR__ . '/../thinkphp/base.php';
+
+// 支持事先使用静态方法设置Request对象和Config对象
+
+// 执行应用并响应
+Container::get('app')->run()->send();
