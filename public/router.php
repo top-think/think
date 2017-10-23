18f1cf51d5833b72db4ad52997cd0e5107e9b09b
thinkphp <thinkphp@qq.com>
2017-10-23 14:36:26 +0800

    修正router

--- public/router.php
+++ public/router.php
@@ -10,7 +10,7 @@
 // +----------------------------------------------------------------------
 // $Id$
 
-if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"])) {
+if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["SCRIPT_NAME"])) {
     return false;
 } else {
     require __DIR__ . "/index.php";
