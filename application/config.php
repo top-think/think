6089b6dac0b01de95b33e4064a9317626904720b
thinkphp <thinkphp@qq.com>
2018-04-14 12:54:13 +0800

    配置调整

--- application/config.php
+++ application/config.php
@@ -122,6 +122,8 @@ return [
     'template'               => [
         // 模板引擎类型 支持 php think 支持扩展
         'type'         => 'Think',
+        // 默认模板渲染规则 1 解析为小写+下划线 2 全部转换小写
+        'auto_rule'    => 1,
         // 模板路径
         'view_path'    => '',
         // 模板后缀
