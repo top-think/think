1d1ad99acd57ddeedd78521abac69ab6ef06e51e
thinkphp <thinkphp@qq.com>
2018-09-05 11:22:55 +0800

    配置文件更新

--- config/console.php
+++ config/console.php
@@ -13,7 +13,8 @@
 // | 控制台配置
 // +----------------------------------------------------------------------
 return [
-    'name'    => 'Think Console',
-    'version' => '0.1',
-    'user'    => null,
+    'name'      => 'Think Console',
+    'version'   => '0.1',
+    'user'      => null,
+    'auto_path' => env('app_path') . 'command' . DIRECTORY_SEPARATOR,
 ];
