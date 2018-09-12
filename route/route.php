a416c37444673b5aee90cc2fad0952e1623f5c81
thinkphp <thinkphp@qq.com>
2018-09-12 22:07:10 +0800

    配置文件调整

--- route/route.php
+++ route/route.php
@@ -8,13 +8,10 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
+use think\facade\Route;
 
 Route::get('think', function () {
     return 'hello,ThinkPHP5!';
 });
 
 Route::get('hello/:name', 'index/hello');
-
-return [
-
-];
