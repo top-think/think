9e43ac2d4740718886edb6d968387a11a854c8d2
thinkphp <thinkphp@qq.com>
2018-11-06 13:14:11 +0800

    修正配置

--- config/cookie.php
+++ config/cookie.php
@@ -24,7 +24,7 @@ return [
     //  cookie 启用安全传输
     'secure'    => false,
     // httponly设置
-    'httponly'  => '',
+    'httponly'  => false,
     // 是否使用 setcookie
     'setcookie' => true,
 ];
