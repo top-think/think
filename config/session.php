3eab5158b4f19abffdf42b0299628cc37945ec58
thinkphp <thinkphp@qq.com>
2018-11-06 16:00:38 +0800

    更新配置参数

--- config/session.php
+++ config/session.php
@@ -17,10 +17,11 @@ return [
     'id'             => '',
     // SESSION_ID的提交变量,解决flash上传跨域
     'var_session_id' => '',
-    // SESSION 前缀
-    'prefix'         => 'think',
     // 驱动方式 支持redis memcache memcached
     'type'           => '',
     // 是否自动开启 SESSION
     'auto_start'     => true,
+    // Session配置参数
+    'options'        => [
+    ],
 ];
--- convention.php
+++ convention.php
@@ -207,14 +207,13 @@ return [
         'id'             => '',
         // SESSION_ID的提交变量,解决flash上传跨域
         'var_session_id' => '',
-        // SESSION 前缀
-        'prefix'         => 'think',
         // 驱动方式 支持redis memcache memcached
         'type'           => '',
         // 是否自动开启 SESSION
         'auto_start'     => true,
-        'httponly'       => true,
-        'secure'         => false,
+        // Session配置参数
+        'options'        => [
+        ],
     ],
 
     // +----------------------------------------------------------------------
