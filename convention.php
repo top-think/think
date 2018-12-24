6b771e06b63de7cd9bdbc5c2045d2d4a70897e26
thinkphp <thinkphp@qq.com>
2018-12-24 21:56:05 +0800

    删除无用的配置参数

--- convention.php
+++ convention.php
@@ -8,116 +8,112 @@ return [
     // +----------------------------------------------------------------------
     'app'      => [
         // 应用名称
-        'app_name'               => '',
+        'app_name'              => '',
         // 应用地址
-        'app_host'               => '',
+        'app_host'              => '',
         // 应用调试模式（环境变量优先）
-        'app_debug'              => false,
+        'app_debug'             => false,
         // 应用Trace（环境变量优先）
-        'app_trace'              => false,
+        'app_trace'             => false,
         // 应用根命名空间
-        'root_namespace'         => 'app',
+        'root_namespace'        => 'app',
         // 应用命名空间（仅在多应用架构有效 请在具体的应用配置中设置）
-        'app_namespace'          => '',
+        'app_namespace'         => '',
         // 默认输出类型
-        'default_return_type'    => 'html',
+        'default_return_type'   => 'html',
         // 默认AJAX 数据返回格式,可选json xml ...
-        'default_ajax_return'    => 'json',
+        'default_ajax_return'   => 'json',
         // 默认JSONP格式返回的处理方法
-        'default_jsonp_handler'  => 'jsonpReturn',
+        'default_jsonp_handler' => 'jsonpReturn',
         // 默认JSONP处理方法
-        'var_jsonp_handler'      => 'callback',
+        'var_jsonp_handler'     => 'callback',
         // 默认时区
-        'default_timezone'       => 'Asia/Shanghai',
+        'default_timezone'      => 'Asia/Shanghai',
         // 是否开启多语言
-        'lang_switch_on'         => false,
+        'lang_switch_on'        => false,
         // 默认全局过滤方法
-        'default_filter'         => [],
+        'default_filter'        => [],
         // 默认语言
-        'default_lang'           => 'zh-cn',
+        'default_lang'          => 'zh-cn',
         // 应用类库后缀
-        'class_suffix'           => false,
-        // 控制器类后缀
-        'controller_suffix'      => false,
+        'class_suffix'          => false,
         // 默认控制器名
-        'default_controller'     => 'Index',
+        'default_controller'    => 'Index',
         // 默认操作名
-        'default_action'         => 'index',
+        'default_action'        => 'index',
         // 默认验证器
-        'default_validate'       => '',
+        'default_validate'      => '',
         // 默认的空控制器名
-        'empty_controller'       => 'Error',
-        // 自动搜索控制器
-        'controller_auto_search' => false,
+        'empty_controller'      => 'Error',
 
         // +----------------------------------------------------------------------
         // | URL及路由设置
         // +----------------------------------------------------------------------
 
         // PATHINFO变量名 用于兼容模式
-        'var_pathinfo'           => 's',
+        'var_pathinfo'          => 's',
         // 兼容PATH_INFO获取
-        'pathinfo_fetch'         => ['ORIG_PATH_INFO', 'REDIRECT_PATH_INFO', 'REDIRECT_URL'],
+        'pathinfo_fetch'        => ['ORIG_PATH_INFO', 'REDIRECT_PATH_INFO', 'REDIRECT_URL'],
         // pathinfo分隔符
-        'pathinfo_depr'          => '/',
+        'pathinfo_depr'         => '/',
         // HTTPS代理标识
-        'https_agent_name'       => '',
+        'https_agent_name'      => '',
         // IP代理获取标识
-        'http_agent_ip'          => 'HTTP_X_REAL_IP',
+        'http_agent_ip'         => 'HTTP_X_REAL_IP',
         // URL伪静态后缀
-        'url_html_suffix'        => 'html',
+        'url_html_suffix'       => 'html',
         // URL普通方式参数 用于自动生成
-        'url_common_param'       => false,
+        'url_common_param'      => false,
         // URL参数方式 0 按名称成对解析 1 按顺序解析
-        'url_param_type'         => 0,
+        'url_param_type'        => 0,
         // 是否开启路由延迟解析
-        'url_lazy_route'         => false,
+        'url_lazy_route'        => false,
         // 是否强制使用路由
-        'url_route_must'         => false,
+        'url_route_must'        => false,
         // 合并路由规则
-        'route_rule_merge'       => false,
+        'route_rule_merge'      => false,
         // 路由是否完全匹配
-        'route_complete_match'   => false,
+        'route_complete_match'  => false,
         // 使用注解路由
-        'route_annotation'       => false,
+        'route_annotation'      => false,
         // 域名根，如thinkphp.cn
-        'url_domain_root'        => '',
+        'url_domain_root'       => '',
         // 是否自动转换URL中的控制器和操作名
-        'url_convert'            => true,
+        'url_convert'           => true,
         // 默认的路由变量规则
-        'default_route_pattern'  => '\w+',
+        'default_route_pattern' => '\w+',
         // 默认的访问控制器层
-        'url_controller_layer'   => 'controller',
+        'url_controller_layer'  => 'controller',
         // 表单请求类型伪装变量
-        'var_method'             => '_method',
+        'var_method'            => '_method',
         // 表单ajax伪装变量
-        'var_ajax'               => '_ajax',
+        'var_ajax'              => '_ajax',
         // 表单pjax伪装变量
-        'var_pjax'               => '_pjax',
+        'var_pjax'              => '_pjax',
         // 是否开启请求缓存 true自动缓存 支持设置请求缓存规则
-        'request_cache'          => false,
+        'request_cache'         => false,
         // 请求缓存有效期
-        'request_cache_expire'   => null,
+        'request_cache_expire'  => null,
         // 全局请求缓存排除规则
-        'request_cache_except'   => [],
+        'request_cache_except'  => [],
 
         // 默认跳转页面对应的模板文件
-        'dispatch_success_tmpl'  => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
-        'dispatch_error_tmpl'    => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
+        'dispatch_success_tmpl' => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
+        'dispatch_error_tmpl'   => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
 
         // +----------------------------------------------------------------------
         // | 异常及错误设置
         // +----------------------------------------------------------------------
 
         // 异常页面的模板文件
-        'exception_tmpl'         => Env::get('think_path') . 'tpl/think_exception.tpl',
+        'exception_tmpl'        => Env::get('think_path') . 'tpl/think_exception.tpl',
 
         // 错误显示信息,非调试模式有效
-        'error_message'          => '页面错误！请稍后再试～',
+        'error_message'         => '页面错误！请稍后再试～',
         // 显示错误信息
-        'show_error_msg'         => false,
+        'show_error_msg'        => false,
         // 异常处理handle类 留空使用 \think\exception\Handle
-        'exception_handle'       => '',
+        'exception_handle'      => '',
     ],
 
     // +----------------------------------------------------------------------
