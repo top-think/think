0ff862765b3a83a8b8d5939a22fb5e3d24f73ca2
thinkphp <thinkphp@qq.com>
2019-02-18 16:02:36 +0800

    配置文件调整

---
+++ config/route.php
@@ -0,0 +1,61 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// +----------------------------------------------------------------------
+// | 应用设置
+// +----------------------------------------------------------------------
+
+return [
+    // PATHINFO变量名 用于兼容模式
+    'var_pathinfo'         => 's',
+    // 兼容PATH_INFO获取
+    'pathinfo_fetch'       => ['ORIG_PATH_INFO', 'REDIRECT_PATH_INFO', 'REDIRECT_URL'],
+    // pathinfo分隔符
+    'pathinfo_depr'        => '/',
+    // HTTPS代理标识
+    'https_agent_name'     => '',
+    // URL伪静态后缀
+    'url_html_suffix'      => 'html',
+    // URL普通方式参数 用于自动生成
+    'url_common_param'     => false,
+    // 是否开启路由延迟解析
+    'url_lazy_route'       => false,
+    // 是否强制使用路由
+    'url_route_must'       => false,
+    // 合并路由规则
+    'route_rule_merge'     => false,
+    // 路由是否完全匹配
+    'route_complete_match' => false,
+    // 使用注解路由
+    'route_annotation'     => false,
+    // 域名根，如thinkphp.cn
+    'url_domain_root'      => '',
+    // 是否自动转换URL中的控制器和操作名
+    'url_convert'          => true,
+    // 表单请求类型伪装变量
+    'var_method'           => '_method',
+    // 表单ajax伪装变量
+    'var_ajax'             => '_ajax',
+    // 表单pjax伪装变量
+    'var_pjax'             => '_pjax',
+    // 是否开启请求缓存 true自动缓存 支持设置请求缓存规则
+    'request_cache'        => false,
+    // 请求缓存有效期
+    'request_cache_expire' => null,
+    // 全局请求缓存排除规则
+    'request_cache_except' => [],
+    // 默认控制器名
+    'default_controller'   => 'Index',
+    // 默认操作名
+    'default_action'       => 'index',
+    // 操作方法后缀
+    'action_suffix'        => '',
+];
--- convention.php
+++ convention.php
@@ -27,67 +27,10 @@ return [
         'default_timezone'        => 'Asia/Shanghai',
         // 是否开启多语言
         'lang_switch_on'          => false,
-        // 默认全局过滤方法
-        'default_filter'          => [],
         // 默认语言
         'default_lang'            => 'zh-cn',
-        // 默认控制器名
-        'default_controller'      => 'Index',
-        // 默认操作名
-        'default_action'          => 'index',
         // 默认验证器
         'default_validate'        => '',
-
-        // +----------------------------------------------------------------------
-        // | URL及路由设置
-        // +----------------------------------------------------------------------
-
-        // PATHINFO变量名 用于兼容模式
-        'var_pathinfo'            => 's',
-        // 兼容PATH_INFO获取
-        'pathinfo_fetch'          => ['ORIG_PATH_INFO', 'REDIRECT_PATH_INFO', 'REDIRECT_URL'],
-        // pathinfo分隔符
-        'pathinfo_depr'           => '/',
-        // HTTPS代理标识
-        'https_agent_name'        => '',
-        // IP代理获取标识
-        'http_agent_ip'           => 'HTTP_X_REAL_IP',
-        // URL伪静态后缀
-        'url_html_suffix'         => 'html',
-        // URL普通方式参数 用于自动生成
-        'url_common_param'        => false,
-        // URL参数方式 0 按名称成对解析 1 按顺序解析
-        'url_param_type'          => 0,
-        // 是否开启路由延迟解析
-        'url_lazy_route'          => false,
-        // 是否强制使用路由
-        'url_route_must'          => false,
-        // 合并路由规则
-        'route_rule_merge'        => false,
-        // 路由是否完全匹配
-        'route_complete_match'    => false,
-        // 使用注解路由
-        'route_annotation'        => false,
-        // 域名根，如thinkphp.cn
-        'url_domain_root'         => '',
-        // 是否自动转换URL中的控制器和操作名
-        'url_convert'             => true,
-        // 默认的路由变量规则
-        'default_route_pattern'   => '\w+',
-        // 默认的访问控制器层
-        'url_controller_layer'    => 'controller',
-        // 表单请求类型伪装变量
-        'var_method'              => '_method',
-        // 表单ajax伪装变量
-        'var_ajax'                => '_ajax',
-        // 表单pjax伪装变量
-        'var_pjax'                => '_pjax',
-        // 是否开启请求缓存 true自动缓存 支持设置请求缓存规则
-        'request_cache'           => false,
-        // 请求缓存有效期
-        'request_cache_expire'    => null,
-        // 全局请求缓存排除规则
-        'request_cache_except'    => [],
 
         // 默认跳转页面对应的模板文件
         'dispatch_success_tmpl'   => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
@@ -99,14 +42,70 @@ return [
 
         // 异常页面的模板文件
         'exception_tmpl'          => Env::get('think_path') . 'tpl/think_exception.tpl',
-
         // 错误显示信息,非调试模式有效
         'error_message'           => '页面错误！请稍后再试～',
         // 显示错误信息
         'show_error_msg'          => false,
         // 异常处理handle类 留空使用 \think\exception\Handle
         'exception_handle'        => '',
+        // 异常响应输出类型
         'exception_response_type' => 'html',
+    ],
+
+    // +----------------------------------------------------------------------
+    // | URL及路由设置
+    // +----------------------------------------------------------------------
+    'route'    => [
+        // HTTPS代理标识
+        'https_agent_name'      => '',
+        // IP代理获取标识
+        'http_agent_ip'         => 'HTTP_X_REAL_IP',
+        // PATHINFO变量名 用于兼容模式
+        'var_pathinfo'          => 's',
+        // 兼容PATH_INFO获取
+        'pathinfo_fetch'        => ['ORIG_PATH_INFO', 'REDIRECT_PATH_INFO', 'REDIRECT_URL'],
+        // pathinfo分隔符
+        'pathinfo_depr'         => '/',
+        // URL伪静态后缀
+        'url_html_suffix'       => 'html',
+        // URL普通方式参数 用于自动生成
+        'url_common_param'      => false,
+        // 是否开启路由延迟解析
+        'url_lazy_route'        => false,
+        // 是否强制使用路由
+        'url_route_must'        => false,
+        // 合并路由规则
+        'route_rule_merge'      => false,
+        // 路由是否完全匹配
+        'route_complete_match'  => false,
+        // 使用注解路由
+        'route_annotation'      => false,
+        // 域名根，如thinkphp.cn
+        'url_domain_root'       => '',
+        // 是否自动转换URL中的控制器和操作名
+        'url_convert'           => true,
+        // 默认的路由变量规则
+        'default_route_pattern' => '\w+',
+        // 表单请求类型伪装变量
+        'var_method'            => '_method',
+        // 表单ajax伪装变量
+        'var_ajax'              => '_ajax',
+        // 表单pjax伪装变量
+        'var_pjax'              => '_pjax',
+        // 是否开启请求缓存 true自动缓存 支持设置请求缓存规则
+        'request_cache'         => false,
+        // 请求缓存有效期
+        'request_cache_expire'  => null,
+        // 全局请求缓存排除规则
+        'request_cache_except'  => [],
+        // 默认全局过滤方法
+        'default_filter'        => [],
+        // 默认控制器名
+        'default_controller'    => 'Index',
+        // 默认操作名
+        'default_action'        => 'index',
+        // 操作方法后缀
+        'action_suffix'         => '',
     ],
 
     // +----------------------------------------------------------------------
