0e11052aebab65fadb0c1be40b75d90c541d8ce4
yunwuxin <448901948@qq.com>
2019-03-12 18:02:21 +0800

    改进

--- config/app.php
+++ config/app.php
@@ -33,18 +33,16 @@ return [
     'default_validate'        => '',
 
     // 默认跳转页面对应的模板文件
-    'dispatch_success_tmpl'   => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
-    'dispatch_error_tmpl'     => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
+    'dispatch_success_tmpl'   => app()->getThinkPath() . 'tpl/dispatch_jump.tpl',
+    'dispatch_error_tmpl'     => app()->getThinkPath() . 'tpl/dispatch_jump.tpl',
 
     // 异常页面的模板文件
-    'exception_tmpl'          => Env::get('think_path') . 'tpl/think_exception.tpl',
+    'exception_tmpl'          => app()->getThinkPath() . 'tpl/think_exception.tpl',
 
     // 错误显示信息,非调试模式有效
     'error_message'           => '页面错误！请稍后再试～',
     // 显示错误信息
     'show_error_msg'          => false,
-    // 异常处理handle类 留空使用 \think\exception\Handle
-    'exception_handle'        => '',
     // 异常响应输出类型
     'exception_response_type' => 'html',
 ];
--- public/index.php
+++ public/index.php
@@ -15,6 +15,8 @@ namespace think;
 require __DIR__ . '/../vendor/autoload.php';
 
 // 执行应用并响应
-(new Web())->initialize()
-    ->run()
-    ->send();
+$app = (new App())->initialize();
+
+/** @var Web $web */
+$web = $app->make(Web::class);
+$web->controllerSuffix()->run()->send();
