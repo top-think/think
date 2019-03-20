d4fdfd548604585fb2441491f46ac37b6780e2a0
thinkphp <thinkphp@qq.com>
2019-03-20 11:51:57 +0800

    调整依赖

--- config/route.php
+++ config/route.php
@@ -60,10 +60,6 @@ return [
     'default_action'        => 'index',
     // 操作方法后缀
     'action_suffix'         => '',
-    // 默认输出类型
-    'default_return_type'   => 'html',
-    // 默认AJAX 数据返回格式,可选json xml ...
-    'default_ajax_return'   => 'json',
     // 默认JSONP格式返回的处理方法
     'default_jsonp_handler' => 'jsonpReturn',
     // 默认JSONP处理方法
