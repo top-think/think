5b7915ecd627c7efb8af5a0c20d382c732adda9c
thinkphp <thinkphp@qq.com>
2019-03-30 14:55:06 +0800

    入口文件改进

--- public/index.php
+++ public/index.php
@@ -15,10 +15,4 @@ namespace think;
 require __DIR__ . '/../vendor/autoload.php';
 
 // 执行HTTP应用并响应
-$http = (new App())->http;
-
-$response = $http->run();
-
-$response->send();
-
-$http->end($response);
+(new App())->http->run()->send();
