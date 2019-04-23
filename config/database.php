64382bc1280f0d84b8f6279d8588cce6f2b2db30
chentaotao <chentaotao@mafengwo.com>
2019-04-23 16:34:03 +0800

    名称错误

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     // 服务器地址
     'hostname'        => Env::get('database.hostname', '127.0.0.1'),
     // 数据库名
-    'database'        => Env::get('database.hostname', ''),
+    'database'        => Env::get('database.database', ''),
     // 用户名
     'username'        => Env::get('database.username', 'root'),
     // 密码
