6fcb320f089e7be46106e42bf4c2e66ba7436da0
thinkphp <thinkphp@qq.com>
2019-04-28 17:33:54 +0800

    调整

--- app/controller/BaseController.php
+++ app/controller/BaseController.php
@@ -10,7 +10,7 @@
 // +----------------------------------------------------------------------
 declare (strict_types = 1);
 
-namespace think;
+namespace app\controller;
 
 use think\exception\HttpResponseException;
 use think\exception\ValidateException;
