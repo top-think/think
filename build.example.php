e16898a19079827e96df6a71fc2638b47663f491
thinkphp <thinkphp@qq.com>
2019-04-30 18:44:14 +0800

    应用自动生成的目录结构文件示例

--- build.example.php
+++ build.example.php
@@ -10,17 +10,17 @@
 // +----------------------------------------------------------------------
 
 /**
- * php think build 自动生成应用目录结构的定义示例
+ * php think build 自动生成应用的目录结构的定义示例
  */
 return [
-    // 定义demo应用的自动生成
-    'demo' => [
-        '__file__'   => ['common.php'],
-        '__dir__'    => ['behavior', 'controller', 'model', 'view'],
-        'controller' => ['Index', 'Test', 'UserType'],
-        'model'      => ['User', 'UserType'],
-        'view'       => ['index/index'],
-    ],
-
-    // 其他更多的应用定义
+    // 需要自动创建的文件
+    '__file__'   => [],
+    // 需要自动创建的目录
+    '__dir__'    => ['controller', 'model', 'view'],
+    // 需要自动创建的控制器
+    'controller' => ['Index'],
+    // 需要自动创建的模型
+    'model'      => ['User'],
+    // 需要自动创建的模板
+    'view'       => ['index/index'],
 ];
