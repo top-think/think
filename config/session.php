9e065467be913d602eaf7164fd2c5b690a69a075
thinkphp <thinkphp@qq.com>
2019-05-13 17:52:36 +0800

    调整session配置

--- config/session.php
+++ config/session.php
@@ -19,9 +19,7 @@ return [
     // SESSION_ID的提交变量,解决flash上传跨域
     'var_session_id' => '',
     // 驱动方式 支持file redis memcache memcached
-    'type'           => '',
-    // 是否自动开启 SESSION
-    'auto_start'     => true,
+    'type'           => 'file',
     // 过期时间
     'expire'         => 0,
 ];
