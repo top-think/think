0d798281d4a6ac7ec1d298cd811ea1556ca36f4a
thinkphp <thinkphp@qq.com>
2019-05-13 18:13:05 +0800

    默认不开启任何系统中间件 由项目自己定义

--- app/middleware.php
+++ app/middleware.php
@@ -1,7 +1,12 @@
 <?php
 
 return [
-    'think\middleware\LoadLangPack',
-    'think\middleware\SessionInit',
-    'think\middleware\TraceDebug',
+    // 全局请求缓存
+    // 'think\middleware\CheckRequestCache',
+    // 多语言加载
+    // 'think\middleware\LoadLangPack',
+    // Session初始化
+    // 'think\middleware\SessionInit',
+    // 页面Trace调试
+    // 'think\middleware\TraceDebug',
 ];
