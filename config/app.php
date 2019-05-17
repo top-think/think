04546c55d2aae2c7c917997bc4dd0eb0f4471267
thinkphp <thinkphp@qq.com>
2019-05-17 16:21:00 +0800

    app配置调整 取消不再使用的dispatch_success_tmpl和dispatch_error_tmpl参数

--- config/app.php
+++ config/app.php
@@ -17,39 +17,35 @@ use think\facade\Env;
 
 return [
     // 应用地址
-    'app_host'              => Env::get('app.host', ''),
+    'app_host'         => Env::get('app.host', ''),
     // 应用Trace（环境变量优先读取）
-    'app_trace'             => false,
+    'app_trace'        => false,
     // 应用的命名空间
-    'app_namespace'         => '',
+    'app_namespace'    => '',
     // 是否启用路由
-    'with_route'            => true,
+    'with_route'       => true,
     // 是否启用事件
-    'with_event'            => true,
+    'with_event'       => true,
     // 自动多应用模式
-    'auto_multi_app'        => false,
+    'auto_multi_app'   => false,
     // 应用映射（自动多应用模式有效）
-    'app_map'               => [],
+    'app_map'          => [],
     // 域名绑定（自动多应用模式有效）
-    'domain_bind'           => [],
+    'domain_bind'      => [],
     // 禁止URL访问的应用列表（自动多应用模式有效）
-    'deny_app_list'         => [],
+    'deny_app_list'    => [],
     // 默认应用
-    'default_app'           => 'index',
+    'default_app'      => 'index',
     // 默认时区
-    'default_timezone'      => 'Asia/Shanghai',
+    'default_timezone' => 'Asia/Shanghai',
     // 默认验证器
-    'default_validate'      => '',
-
-    // 默认跳转页面对应的模板文件
-    'dispatch_success_tmpl' => app()->getThinkPath() . 'tpl/dispatch_jump.tpl',
-    'dispatch_error_tmpl'   => app()->getThinkPath() . 'tpl/dispatch_jump.tpl',
+    'default_validate' => '',
 
     // 异常页面的模板文件
-    'exception_tmpl'        => app()->getThinkPath() . 'tpl/think_exception.tpl',
+    'exception_tmpl'   => app()->getThinkPath() . 'tpl/think_exception.tpl',
 
     // 错误显示信息,非调试模式有效
-    'error_message'         => '页面错误！请稍后再试～',
+    'error_message'    => '页面错误！请稍后再试～',
     // 显示错误信息
-    'show_error_msg'        => false,
+    'show_error_msg'   => false,
 ];
