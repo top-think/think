296f1e8111afde16f86d162675612425e82fcbda
thinkphp <thinkphp@qq.com>
2019-05-30 11:43:14 +0800

    缓存配置调整

--- config/cache.php
+++ config/cache.php
@@ -15,11 +15,15 @@
 
 return [
     // 驱动方式
-    'type'   => 'File',
+    'type'       => 'File',
     // 缓存保存目录
-    'path'   => '',
+    'path'       => '',
     // 缓存前缀
-    'prefix' => '',
+    'prefix'     => '',
     // 缓存有效期 0表示永久缓存
-    'expire' => 0,
+    'expire'     => 0,
+    // 缓存标签前缀
+    'tag_prefix' => 'tag:',
+    // 序列化机制 例如 ['serialize', 'unserialize']
+    'serialize'  => [],
 ];
