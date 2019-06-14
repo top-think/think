5c41421fe246c93ac3b8566a5b8d4f8043b67bbc
thinkphp <thinkphp@qq.com>
2019-06-14 15:28:24 +0800

    数据库和缓存配置文件格式调整

--- config/database.php
+++ config/database.php
@@ -12,50 +12,58 @@
 use think\facade\Env;
 
 return [
-    // 数据库类型
-    'type'            => Env::get('database.type', 'mysql'),
-    // 服务器地址
-    'hostname'        => Env::get('database.hostname', '127.0.0.1'),
-    // 数据库名
-    'database'        => Env::get('database.database', ''),
-    // 用户名
-    'username'        => Env::get('database.username', 'root'),
-    // 密码
-    'password'        => Env::get('database.password', ''),
-    // 端口
-    'hostport'        => Env::get('database.hostport', '3306'),
-    // 连接dsn
-    'dsn'             => '',
-    // 数据库连接参数
-    'params'          => [],
-    // 数据库编码默认采用utf8
-    'charset'         => Env::get('database.charset', 'utf8'),
-    // 数据库表前缀
-    'prefix'          => Env::get('database.prefix', ''),
-    // 数据库调试模式
-    'debug'           => Env::get('database.debug', true),
-    // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-    'deploy'          => 0,
-    // 数据库读写是否分离 主从式有效
-    'rw_separate'     => false,
-    // 读写分离后 主服务器数量
-    'master_num'      => 1,
-    // 指定从服务器序号
-    'slave_no'        => '',
-    // 是否严格检查字段是否存在
-    'fields_strict'   => true,
+    // 默认使用的数据库连接配置
+    'default'         => Env::get('database.driver', 'mysql'),
+
+    // 数据库连接配置信息
+    'connections'     => [
+        'mysql' => [
+            // 数据库类型
+            'type'            => Env::get('database.type', 'mysql'),
+            // 服务器地址
+            'hostname'        => Env::get('database.hostname', '127.0.0.1'),
+            // 数据库名
+            'database'        => Env::get('database.database', ''),
+            // 用户名
+            'username'        => Env::get('database.username', 'root'),
+            // 密码
+            'password'        => Env::get('database.password', ''),
+            // 端口
+            'hostport'        => Env::get('database.hostport', '3306'),
+            // 连接dsn
+            'dsn'             => '',
+            // 数据库连接参数
+            'params'          => [],
+            // 数据库编码默认采用utf8
+            'charset'         => Env::get('database.charset', 'utf8'),
+            // 数据库表前缀
+            'prefix'          => Env::get('database.prefix', ''),
+            // 数据库调试模式
+            'debug'           => Env::get('database.debug', true),
+            // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
+            'deploy'          => 0,
+            // 数据库读写是否分离 主从式有效
+            'rw_separate'     => false,
+            // 读写分离后 主服务器数量
+            'master_num'      => 1,
+            // 指定从服务器序号
+            'slave_no'        => '',
+            // 是否严格检查字段是否存在
+            'fields_strict'   => true,
+            // 是否需要进行SQL性能分析
+            'sql_explain'     => false,
+            // Builder类
+            'builder'         => '',
+            // Query类
+            'query'           => '',
+            // 是否需要断线重连
+            'break_reconnect' => false,
+        ],
+
+        // 更多的数据库配置信息
+    ],
     // 自动写入时间戳字段
     'auto_timestamp'  => false,
     // 时间字段取出后的默认时间格式
     'datetime_format' => 'Y-m-d H:i:s',
-    // 是否需要进行SQL性能分析
-    'sql_explain'     => false,
-    // Builder类
-    'builder'         => '',
-    // Query类
-    'query'           => '\\think\\db\\Query',
-    // 是否需要断线重连
-    'break_reconnect' => false,
-    // 断线标识字符串
-    'break_match_str' => [],
 ];
