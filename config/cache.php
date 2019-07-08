4928b95be34aef0e08e6b6afa82505e9a40045e5
thinkphp <thinkphp@qq.com>
2019-07-08 17:35:54 +0800

    配置调整

--- config/cache.php
+++ config/cache.php
@@ -24,7 +24,7 @@ return [
             // 驱动方式
             'type'       => 'File',
             // 缓存保存目录
-            'path'       => '',
+            'path'       => app()->getRuntimePath() . 'cache',
             // 缓存前缀
             'prefix'     => '',
             // 缓存有效期 0表示永久缓存
--- config/log.php
+++ config/log.php
@@ -31,7 +31,7 @@ return [
             // 日志记录方式
             'type'           => 'File',
             // 日志保存目录
-            'path'           => '',
+            'path'           => app()->getRuntimePath() . 'log',
             // 单文件日志写入
             'single'         => false,
             // 独立日志级别
