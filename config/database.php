1ed61317728aed2d3c2e720c5fe2a5097881db27
thinkphp <thinkphp@qq.com>
2019-07-24 22:55:31 +0800

    配置更新

--- config/database.php
+++ config/database.php
@@ -30,39 +30,39 @@ return [
     'connections'     => [
         'mysql' => [
             // 数据库类型
-            'type'            => Env::get('database.type', 'mysql'),
+            'type'              => Env::get('database.type', 'mysql'),
             // 服务器地址
-            'hostname'        => Env::get('database.hostname', '127.0.0.1'),
+            'hostname'          => Env::get('database.hostname', '127.0.0.1'),
             // 数据库名
-            'database'        => Env::get('database.database', ''),
+            'database'          => Env::get('database.database', ''),
             // 用户名
-            'username'        => Env::get('database.username', 'root'),
+            'username'          => Env::get('database.username', 'root'),
             // 密码
-            'password'        => Env::get('database.password', ''),
+            'password'          => Env::get('database.password', ''),
             // 端口
-            'hostport'        => Env::get('database.hostport', '3306'),
+            'hostport'          => Env::get('database.hostport', '3306'),
             // 数据库连接参数
-            'params'          => [],
+            'params'            => [],
             // 数据库编码默认采用utf8
-            'charset'         => Env::get('database.charset', 'utf8'),
+            'charset'           => Env::get('database.charset', 'utf8'),
             // 数据库表前缀
-            'prefix'          => Env::get('database.prefix', ''),
+            'prefix'            => Env::get('database.prefix', ''),
             // 数据库调试模式
-            'debug'           => Env::get('database.debug', true),
+            'debug'             => Env::get('database.debug', true),
             // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-            'deploy'          => 0,
+            'deploy'            => 0,
             // 数据库读写是否分离 主从式有效
-            'rw_separate'     => false,
+            'rw_separate'       => false,
             // 读写分离后 主服务器数量
-            'master_num'      => 1,
+            'master_num'        => 1,
             // 指定从服务器序号
-            'slave_no'        => '',
+            'slave_no'          => '',
             // 是否严格检查字段是否存在
-            'fields_strict'   => true,
-            // 是否需要进行SQL性能分析
-            'sql_explain'     => false,
+            'fields_strict'     => true,
             // 是否需要断线重连
-            'break_reconnect' => false,
+            'break_reconnect'   => false,
+            // 字段缓存路径
+            'schema_cache_path' => app()->getRuntimePath() . 'schema' . DIRECTORY_SEPARATOR,
         ],
 
         // 更多的数据库配置信息
