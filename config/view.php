63b96a80465013a4859132f88251166be26b51b2
thinkphp <thinkphp@qq.com>
2019-09-10 23:01:31 +0800

    配置调整

--- config/view.php
+++ config/view.php
@@ -5,23 +5,23 @@
 
 return [
     // 模板引擎类型使用Think
-    'type'         => 'Think',
+    'type'          => 'Think',
     // 默认模板渲染规则 1 解析为小写+下划线 2 全部转换小写 3 保持操作方法
-    'auto_rule'    => 1,
-    // 模板基础路径
-    'view_base'    => '',
+    'auto_rule'     => 1,
+    // 模板目录名
+    'view_dir_name' => 'view',
     // 模板路径
-    'view_path'    => '',
+    'view_path'     => '',
     // 模板后缀
-    'view_suffix'  => 'html',
+    'view_suffix'   => 'html',
     // 模板文件名分隔符
-    'view_depr'    => DIRECTORY_SEPARATOR,
+    'view_depr'     => DIRECTORY_SEPARATOR,
     // 模板引擎普通标签开始标记
-    'tpl_begin'    => '{',
+    'tpl_begin'     => '{',
     // 模板引擎普通标签结束标记
-    'tpl_end'      => '}',
+    'tpl_end'       => '}',
     // 标签库标签开始标记
-    'taglib_begin' => '{',
+    'taglib_begin'  => '{',
     // 标签库标签结束标记
-    'taglib_end'   => '}',
+    'taglib_end'    => '}',
 ];
