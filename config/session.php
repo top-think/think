40cbc072bd4aeb9190253133cccd86a3988e2972
thinkphp <thinkphp@qq.com>
2019-09-11 11:35:25 +0800

    session配置调整

--- config/session.php
+++ config/session.php
@@ -8,8 +8,10 @@ return [
     'name'           => 'PHPSESSID',
     // SESSION_ID的提交变量,解决flash上传跨域
     'var_session_id' => '',
-    // 驱动方式 支持file redis memcache memcached
+    // 驱动方式 支持file cache
     'type'           => 'file',
+    // 存储连接标识 当type使用cache的时候有效
+    'store'          => null,
     // 过期时间
     'expire'         => 1440,
     // 前缀
