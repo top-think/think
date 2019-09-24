7b57391a32ba38393c49c53abec37a9ae08c7652
蒋文健 <jwjbjg@gmail.com>
2019-09-24 09:49:42 +0800

    完善注释

--- config/filesystem.php
+++ config/filesystem.php
@@ -3,16 +3,22 @@
 use think\facade\Env;
 
 return [
+    // 默认磁盘
     'default' => Env::get('filesystem.driver', 'local'),
+    // 磁盘列表
     'disks'   => [
         'local'  => [
             'type' => 'local',
             'root' => app()->getRuntimePath() . 'storage',
         ],
         'public' => [
+            // 磁盘类型
             'type'       => 'local',
+            // 磁盘路径
             'root'       => app()->getRootPath() . 'public/storage',
+            // 磁盘路径对应的外部URL路径
             'url'        => '/storage',
+            // 可见性
             'visibility' => 'public',
         ],
         // 更多的磁盘配置信息
