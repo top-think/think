3785d4b4deeaf4b93307cfd735cd2abeeaf17868
thinkphp <thinkphp@qq.com>
2019-10-02 21:18:53 +0800

    全局中间件调整

--- app/middleware.php
+++ app/middleware.php
@@ -6,7 +6,5 @@ return [
     // 多语言加载
     // \think\middleware\LoadLangPack::class,
     // Session初始化
-    // \think\middleware\SessionInit::class,
-    // 页面Trace调试
-    // \think\middleware\TraceDebug::class,
+    // \think\middleware\SessionInit::class
 ];
