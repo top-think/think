72c953db1e5ca4d512962d2ef1e7326ed1cc6cf8
thinkphp <thinkphp@qq.com>
2019-11-07 12:00:59 +0800

    SQL监听配置调整

--- config/database.php
+++ config/database.php
@@ -1,9 +1,8 @@
 <?php
-use think\facade\Env;
 
 return [
     // 默认使用的数据库连接配置
-    'default'         => Env::get('database.driver', 'mysql'),
+    'default'         => env('database.driver', 'mysql'),
 
     // 自定义时间查询规则
     'time_query_rule' => [],
@@ -20,23 +19,23 @@ return [
     'connections'     => [
         'mysql' => [
             // 数据库类型
-            'type'              => Env::get('database.type', 'mysql'),
+            'type'              => env('database.type', 'mysql'),
             // 服务器地址
-            'hostname'          => Env::get('database.hostname', '127.0.0.1'),
+            'hostname'          => env('database.hostname', '127.0.0.1'),
             // 数据库名
-            'database'          => Env::get('database.database', ''),
+            'database'          => env('database.database', ''),
             // 用户名
-            'username'          => Env::get('database.username', 'root'),
+            'username'          => env('database.username', 'root'),
             // 密码
-            'password'          => Env::get('database.password', ''),
+            'password'          => env('database.password', ''),
             // 端口
-            'hostport'          => Env::get('database.hostport', '3306'),
+            'hostport'          => env('database.hostport', '3306'),
             // 数据库连接参数
             'params'            => [],
             // 数据库编码默认采用utf8
-            'charset'           => Env::get('database.charset', 'utf8'),
+            'charset'           => env('database.charset', 'utf8'),
             // 数据库表前缀
-            'prefix'            => Env::get('database.prefix', ''),
+            'prefix'            => env('database.prefix', ''),
 
             // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
             'deploy'            => 0,
@@ -51,7 +50,7 @@ return [
             // 是否需要断线重连
             'break_reconnect'   => false,
             // 监听SQL
-            'trigger_sql'       => true,
+            'trigger_sql'       => env('app_debug', true),
             // 开启字段缓存
             'fields_cache'      => false,
             // 字段缓存路径
