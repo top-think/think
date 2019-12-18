493a3d0596a18fd78a0f6ada3ae623508d3da4e8
thinkphp <thinkphp@qq.com>
2019-12-18 15:10:08 +0800

    增加AppBootService服务类 便于应用定义

---
+++ app/AppBootService.php
@@ -0,0 +1,15 @@
+<?php
+namespace app;
+
+use think\App;
+
+/**
+ * 应用启动服务类
+ */
+class AppBootService
+{
+    public function init(App $app)
+    {
+        // 系统启动后执行的初始化操作
+    }
+}
---
+++ app/service.php
@@ -0,0 +1,9 @@
+<?php
+
+use app\AppBootService;
+
+// 系统服务定义文件
+// 服务在完成全局初始化之后执行
+return [
+    AppBootService::class,
+];
