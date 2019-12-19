c6e9c6e92b122fd8af4add590edb9c9f0d889c11
thinkphp <thinkphp@qq.com>
2019-12-19 17:08:09 +0800

    调整

---
+++ app/AppService.php
@@ -0,0 +1,22 @@
+<?php
+declare (strict_types = 1);
+
+namespace app;
+
+use think\Service;
+
+/**
+ * 应用服务类
+ */
+class AppService extends Service
+{
+    public function register()
+    {
+        // 服务注册
+    }
+
+    public function boot()
+    {
+        // 服务启动
+    }
+}
--- app/service.php
+++ app/service.php
@@ -1,9 +1,9 @@
 <?php
 
-use app\AppBootService;
+use app\AppService;
 
 // 系统服务定义文件
 // 服务在完成全局初始化之后执行
 return [
-    AppBootService::class,
+    AppService::class,
 ];
