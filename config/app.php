368b58f594577bd0f48caf330f78f256f5849f5a
sy-records <52o@qq52o.cn>
2020-09-12 16:38:42 +0800

    fix: get the default_timezone from env

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     // 默认应用
     'default_app'      => 'index',
     // 默认时区
-    'default_timezone' => 'Asia/Shanghai',
+    'default_timezone' => env('app.default_timezone', 'Asia/Shanghai'),
 
     // 应用映射（自动多应用模式有效）
     'app_map'          => [],
