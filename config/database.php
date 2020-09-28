956b07a5cf47927600a1177d679fe102c49d9790
thinkphp <thinkphp@qq.com>
2020-09-28 17:08:46 +0800

    取消schema_cache_path配置

--- config/database.php
+++ config/database.php
@@ -19,42 +19,40 @@ return [
     'connections'     => [
         'mysql' => [
             // 数据库类型
-            'type'              => env('database.type', 'mysql'),
+            'type'            => env('database.type', 'mysql'),
             // 服务器地址
-            'hostname'          => env('database.hostname', '127.0.0.1'),
+            'hostname'        => env('database.hostname', '127.0.0.1'),
             // 数据库名
-            'database'          => env('database.database', ''),
+            'database'        => env('database.database', ''),
             // 用户名
-            'username'          => env('database.username', 'root'),
+            'username'        => env('database.username', 'root'),
             // 密码
-            'password'          => env('database.password', ''),
+            'password'        => env('database.password', ''),
             // 端口
-            'hostport'          => env('database.hostport', '3306'),
+            'hostport'        => env('database.hostport', '3306'),
             // 数据库连接参数
-            'params'            => [],
+            'params'          => [],
             // 数据库编码默认采用utf8
-            'charset'           => env('database.charset', 'utf8'),
+            'charset'         => env('database.charset', 'utf8'),
             // 数据库表前缀
-            'prefix'            => env('database.prefix', ''),
+            'prefix'          => env('database.prefix', ''),
 
             // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-            'deploy'            => 0,
+            'deploy'          => 0,
             // 数据库读写是否分离 主从式有效
-            'rw_separate'       => false,
+            'rw_separate'     => false,
             // 读写分离后 主服务器数量
-            'master_num'        => 1,
+            'master_num'      => 1,
             // 指定从服务器序号
-            'slave_no'          => '',
+            'slave_no'        => '',
             // 是否严格检查字段是否存在
-            'fields_strict'     => true,
+            'fields_strict'   => true,
             // 是否需要断线重连
-            'break_reconnect'   => false,
+            'break_reconnect' => false,
             // 监听SQL
-            'trigger_sql'       => env('app_debug', true),
+            'trigger_sql'     => env('app_debug', true),
             // 开启字段缓存
-            'fields_cache'      => false,
-            // 字段缓存路径
-            'schema_cache_path' => app()->getRuntimePath() . 'schema' . DIRECTORY_SEPARATOR,
+            'fields_cache'    => false,
         ],
 
         // 更多的数据库配置信息
